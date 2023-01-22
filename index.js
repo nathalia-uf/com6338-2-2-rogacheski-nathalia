7969f152022a155c4270d9c620fae0bfc0c5822e
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-01-21 23:29:20 -0500

    change 1

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 function greet(){
     var Name = prompt('What is your name?')
-    if (Name) {
-        alert('Hello, ' + Name)
-    } else {
-        alert('Hello')
-    }
+    alert('Hello, ' + Name)
     var age = prompt('How old are you?')
     var Ageresult = parseInt(age)
     if (!Ageresult) Number
     var birthday = confirm('Have you had your birthday this year?')
+    if (true) {
+        alert(Date().getFullYear() - Ageresult)
+    } else {
+        alert(Date().getFullYear() - Ageresult +1)
+    }
 }
-
-greet ()
