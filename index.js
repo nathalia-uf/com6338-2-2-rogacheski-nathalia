7fe2076bf6166114112cc6cc303e98a7ec752909
nathalia-uf <112099626+nathalia-uf@users.noreply.github.com>
2023-01-21 23:19:57 -0500

    name age and birthday

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function greet(){
-    prompt(What is your name?)
+    var Name = prompt('What is your name?')
+    if (Name) {
+        alert('Hello, ' + Name)
+    } else {
+        alert('Hello')
+    }
+    var age = prompt('How old are you?')
+    var Ageresult = parseInt(age)
+    if (!Ageresult) Number
+    var birthday = confirm('Have you had your birthday this year?')
 }
+
+greet ()
